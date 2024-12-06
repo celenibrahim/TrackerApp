7a93e6c37d9c0b1362787f2242586658cace8da8
celenibrahim <celen.ibrahim@outlook.com>
2024-12-06 19:25:44 +0300

    added react native elements and styled signup page

--- App.js
+++ App.js
@@ -10,10 +10,17 @@ import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
 
 const switchNavigator = createSwitchNavigator({
-  loginFlow: createStackNavigator({
-    Signup: SignupScreen,
-    Signin: SigninScreen,
-  }),
+  loginFlow: createStackNavigator(
+    {
+      Signup: SignupScreen,
+      Signin: SigninScreen,
+    },
+    {
+      defaultNavigationOptions: {
+        headerShown: false, // Tüm ekranlar için header'ı gizle
+      },
+    }
+  ),
   mainFlow: createMaterialBottomTabNavigator({
     trackListFlow: createStackNavigator({
       TrackList: TrackListScreen,
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,44 @@
-import { StyleSheet, Text, View, Button } from "react-native";
-import React from "react";
-
+import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>SignupScreen</Text>
-      <Button
-        title="go to sigin"
-        onPress={() => navigation.navigate("Signin")}
-      />
-      <Button
-        title="go to mainflow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+        <Spacer>
+          <Input
+            label="Email"
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </Spacer>
+        <Spacer>
+          <Input
+            secureTextEntry
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </Spacer>
+        <Button title="Sign Up"></Button>
+      </Spacer>
     </View>
   );
 };
-
 export default SignupScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
